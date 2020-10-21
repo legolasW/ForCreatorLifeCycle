fe43721d8d62f377cfe9bf9e57a6689ce46f2ebc
Legolas <057@live.com>
2020-10-21 16:19:48 -0700

    Initial Commit

---
+++ Shared/ForCreatorLifeCycleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForCreatorLifeCycleApp.swift
+//  Shared
+//
+//  Created by Legolas on 10/21/20.
+//
+
+import SwiftUI
+
+@main
+struct ForCreatorLifeCycleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
