4ef315e9a6f94609c6ab637f2e3cec4365a7ab04
Legolas <057@live.com>
2020-10-22 06:02:29 -0700

    App 生命周期

--- Shared/ForCreatorLifeCycleApp.swift
+++ Shared/ForCreatorLifeCycleApp.swift
@@ -9,9 +9,26 @@ import SwiftUI
 
 @main
 struct ForCreatorLifeCycleApp: App {
+    @Environment(\.scenePhase) private var scenePhase
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+        .onChange(of: scenePhase) { newPhase in
+            switch newPhase {
+            case .active:
+                print("Foreground Active")
+            case .inactive:
+                print("Foreground Inactive")
+            case .background:
+                print("Background")
+            @unknown default:
+                print("Error ")
+            }
+        }
     }
 }
+
+
+
